2977ad74cb42ec8d93ed02e918944ce03dc7a63b
bien aime <bybienaime@gmmail.com>
2020-09-03 05:31:46 +0200

    add login and protected route middleware

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const mongoose = require("mongoose")
 var dotenv = require("dotenv")
 const graphqlSchema = require("./graphql/schema/index")
 const graphqlResolvers = require("./graphql/resolvers/index")
+const isAuth = require("./middleware/protectRoute")
 
 dotenv.config()
 const app = express()
 
 app.use(bodyParser.json())
+
+app.use(isAuth)
 
 app.use(
   "/graphql",
@@ -24,7 +27,7 @@ mongoose
   .connect(
     process.env.MONGOLAB_URI,
 
-    {useNewUrlParser: true}
+    {useNewUrlParser: true, useUnifiedTopology: true}
   )
   .then(() => {
     app.listen(3000, () => console.log("Server started"))
